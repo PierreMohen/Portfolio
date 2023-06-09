9941d75c5394183edd733ed5b0503722d5abeb48
PierreMohen <50109887+PierreMohen@users.noreply.github.com>
2023-06-09 17:45:47 +0200

    myportfolio m

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faWarning } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector:'app-home',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  warning = faWarning;
 }
